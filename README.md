eb89f15d133070190de14300d2e4e048b85322e5
Jeff Kehres <jeff.kehres@gmail.com>
2017-07-13 17:24:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# memcached-elasticache
+
+Drop-in replacement for [`memcached`](https://github.com/3rd-Eden/memcached) module (a fully featured Memcached client for Node.js) that adds support for [auto-discovery](http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/AutoDiscovery.html) of nodes in an AWS ElastiCache cluster running the Memcached engine. Works with all AWS supported cache engine versions.
+
+## Installation
+
+`npm install memcached-elasticache`
+
+## Setting up the client
+
+The constructor of the client takes two arguments `server locations` and `options`:
+
+```js
+const Memcached = require('memcached-elasticache');
+const memcached = new Memcached(Server locations, options);
+```
+
+### Server locations
+
+When auto-discovery is enabled (default), specify the configuration endpoint of the cluster as a string in the following format: `hostname:port`. Nodes will be automatically detected/registered with the client in this case.
+
+When auto-discovery is disabled, same as [`memcached`](https://github.com/3rd-Eden/memcached) module - specify one or more nodes that make up your cluster.
+
+### Options
+
+Same as [`memcached`](https://github.com/3rd-Eden/memcached) module with the following extra options:
+
+#### autoDiscover
+
+A flag indicating whether the client should automatically discover the nodes of the cluster or not. If false, client behaviour is identical to [`memcached`](https://github.com/3rd-Eden/memcached) module and you must manually specify the nodes of the cluster when constructing the client. Defaults to `true`.
+
+#### autoDiscoverInterval
+
+The number of milliseconds between attempts to discover changes to the cluster - added/removed nodes. When auto-discovery is disabled, no polling occurs and this value is ignored. Defaults to `60000` (i.e. on minute).
+
+## API
+
+Same as [`memcached`](https://github.com/3rd-Eden/memcached) module - except private methods are not exposed.
+
+## Events
+
+Same as [`memcached`](https://github.com/3rd-Eden/memcached) module with the following extra events:
+
+### 'autoDiscover'
+
+Emitted when a change to the cluster is detected - added/removed nodes. Payload is an array of server locations for the current nodes in the cluster. When auto-discovery is disabled, this event is not emitted.
+
+### 'autoDiscoverFailure'
+
+Emitted when a error occurs while attempting to check for changes to the cluster. Payload is an `Error` object. When auto-discovery is disabled, this event is not emitted.
---
+++ index.js
@@ -0,0 +1,153 @@
+'use strict';
+
+const EventEmitter = require('events');
+const Memcached = require('memcached');
+const _ = require('lodash');
+
+const GET_CLUSTER_COMMAND_OLD = 'get AmazonElastiCache:cluster';
+const GET_CLUSTER_COMMAND_NEW = 'config get cluster';
+
+const DEFAULT_OPTIONS = {
+    autoDiscover: true,
+    autoDiscoverInterval: 60000
+};
+
+class Client extends EventEmitter {
+
+    constructor(configEndpoint, options) {
+        super();
+
+        this._options = Object.assign({}, DEFAULT_OPTIONS, options);
+        this._configEndpoint = configEndpoint;
+        this._nodes = [];
+
+        // when auto-discovery is enabled, the configuration endpoint is a valid
+        // cluster node so use it to for the initial inner client until cluser
+        // discovery is complete and the inner client is replaced/updated with
+        // all the nodes in the cluster; when auto-discovery is disabled, the
+        // inner client never changes and this class is just a dumb wrapper
+        this._createInnerClient(configEndpoint);
+
+        // start auto-discovery, if enabled
+        if (this._options.autoDiscover) {
+            this._getCluster();
+            this._timer = setInterval(this._getCluster.bind(this), this._options.autoDiscoverInterval);
+        }
+    }
+
+    end() {
+
+        // stop auto-discovery
+        if (this._timer) {
+            clearInterval(this._timer);
+            this.timer = null;
+        }
+
+        this._innerClient.end();
+    }
+
+    _getCluster() {
+
+        // connect to configuration endpoint
+        const configClient = new Memcached(this._configEndpoint, this._options);
+
+        new Promise((resolve, reject) => {
+
+            // get cache engine version
+            configClient.version((err, version) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(version);
+                }
+            });
+        })
+        .then((version) => {
+
+            // select cluster command based on cache engine version
+            const major = parseInt(version[0].major);
+            const minor = parseInt(version[0].minor);
+            const bugfix = parseInt(version[0].bugfix);
+            const clusterCommand =
+                (major > 1) || (major === 1 && minor > 4) || (major === 1 && minor === 4 && bugfix >= 14) ?
+                GET_CLUSTER_COMMAND_NEW : GET_CLUSTER_COMMAND_OLD;
+
+            // request nodes from configuration endpoint
+            return new Promise((resolve, reject) => {
+                configClient.command(() => {
+                    return {
+                        command: clusterCommand,
+                        callback: (err, data) => {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve(data);
+                            }
+                        }
+                    };
+                });
+            });
+        })
+        .then((data) => this._parseNodes(data))
+        .then((nodes) => {
+
+			// don't update inner client if nodes have not changed
+            if (!_.isEqual(this._nodes, nodes)) {
+                this._nodes = nodes;
+                this._createInnerClient(nodes);
+                this.emit('autoDiscover', nodes);
+            }
+            configClient.end();
+        })
+        .catch((err) => {
+            this.emit('autoDiscoverFailure', err);
+            configClient.end();
+        })
+    }
+
+    _parseNodes(data) {
+        return data.split('\n')[1].split(' ').map((entry) => {
+            const parts = entry.split('|');
+            return `${parts[0]}:${parts[2]}`;
+        }).sort();
+    }
+
+    _createInnerClient(servers) {
+
+        // (re)create inner client object - do not call end() on previous inner
+        // client as this will cancel any in-flight operations
+        this._innerClient = new Memcached(servers, this.options);
+
+        // passthrough method calls from outer object to inner object - except
+        // end(), which we explicitly override
+        [
+            'touch',
+            'get',
+            'gets',
+            'getMulti',
+            'set',
+            'replace',
+            'add',
+            'cas',
+            'append',
+            'prepend',
+            'incr',
+            'decr',
+            'del',
+            'version',
+            'flush',
+            'stats',
+            'settings',
+            'slabs',
+            'items',
+            'cachedump'
+        ].forEach((func) => {
+            this[func] = this._innerClient[func].bind(this._innerClient);
+        });
+
+        // passthrough emitted events from inner object to outer object
+        this._innerClient.emit = this.emit.bind(this);
+    }
+}
+
+module.exports = Client;
